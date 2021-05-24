dd9c70f209fb2d65200e6a552060b4db6cf9bc84
bcy <bcygsws@163.com>
2021-05-25 02:39:04 +0800

    本地宝项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   rules: {
+    // 在方法名和括号之间要有空格，忽略检查
+    'space-before-function-paren': 0,
+    // 每个语句后，否则报错，必须一直有分号
+    semi: ['error', 'always'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};
